281aeecf638929e4f58999fd513a8d9e620bdf2d
Flisan <me@f4n.dev>
2025-03-22 11:45:45 +0900

    fix: for later

--- workspaces/schema/src/openapi/schema.ts
+++ workspaces/schema/src/openapi/schema.ts
@@ -177,6 +177,7 @@ export const getProgramByIdResponse = program.extend({
 // GET /recommended/:referenceId
 export const getRecommendedModulesRequestParams = z.object({
   referenceId: z.string(),
+  limit: z.number().optional(),
 });
 export const getRecommendedModulesResponse = z.array(
   recommendedModule.extend({
