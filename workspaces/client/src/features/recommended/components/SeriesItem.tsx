19677d0e480034a69db4198aa0f646343161fdb1
Koki Miita <miita@c-style.net>
2025-03-22 20:55:13 +0900

    feat: lazy loading image (finally)

--- workspaces/client/src/features/recommended/components/SeriesItem.tsx
+++ workspaces/client/src/features/recommended/components/SeriesItem.tsx
@@ -2,6 +2,7 @@ import { Flipped } from 'react-flip-toolkit';
 import { NavLink } from 'react-router';
 
 import { Hoverable } from '@wsh-2025/client/src/features/layout/components/Hoverable';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 interface Props {
   series: {
@@ -20,7 +21,12 @@ export const SeriesItem = ({ series }: Props) => {
             <>
               <div className="relative overflow-hidden rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]">
                 <Flipped stagger flipId={isTransitioning ? `series-${series.id}` : 0}>
-                  <img alt="" className="h-auto w-full" src={series.thumbnailUrl.replace("?", "-400px.webp?")} />
+                  <LazyLoadImage
+                    alt=""
+                    className="aspect-video !block"
+                    src={series.thumbnailUrl.replace("?", "-400px.webp?")}
+                    width="100%"
+                  />
                 </Flipped>
               </div>
               <div className="p-[8px]">
