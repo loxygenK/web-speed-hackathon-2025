070f9145505a4639007e7b1d0113199097ca0a90
Koki Miita <miita@c-style.net>
2025-03-23 13:09:47 +0900

    fix: reduce rerendering

--- workspaces/client/src/features/layout/hooks/useSubscribePointer.ts
+++ workspaces/client/src/features/layout/hooks/useSubscribePointer.ts
@@ -3,25 +3,15 @@ import { useEffect } from 'react';
 import { useStore } from '@wsh-2025/client/src/app/StoreContext';
 
 export function useSubscribePointer(): void {
-  const s = useStore((s) => s);
+  const updatePointer = useStore((s) => s.features.layout.updatePointer);
 
   useEffect(() => {
     const abortController = new AbortController();
 
-    const current = { x: 0, y: 0 };
     const handlePointerMove = (ev: MouseEvent) => {
-      current.x = ev.clientX;
-      current.y = ev.clientY;
+      updatePointer({ x: ev.clientX, y: ev.clientY });
     };
     window.addEventListener('pointermove', handlePointerMove, { signal: abortController.signal });
-
-    let immediate = setTimeout(function tick() {
-      s.features.layout.updatePointer({ ...current });
-      immediate = setTimeout(tick);
-    }, 0);
-    abortController.signal.addEventListener('abort', () => {
-      clearTimeout(immediate);
-    });
 
     return () => {
       abortController.abort();
