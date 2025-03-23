5bde9e5a3f18cac495cb75348772dfa8d995a9d2
Koki Miita <miita@c-style.net>
2025-03-23 14:08:05 +0900

    feat: pointer does not have to be global state

--- workspaces/client/src/features/layout/hooks/usePointer.ts
+++ workspaces/client/src/features/layout/hooks/usePointer.ts
@@ -1,6 +1,20 @@
-import { useStore } from '@wsh-2025/client/src/app/StoreContext';
+import { useEffect, useState } from 'react';
 
 export function usePointer(): { x: number; y: number } {
-  const s = useStore((s) => s);
-  return s.features.layout.pointer;
+  const [pos, setPos] = useState<[number, number]>([0, 0]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    const handlePointerMove = (ev: MouseEvent) => {
+      setPos([ev.clientX, ev.clientY]);
+    };
+    window.addEventListener('pointermove', handlePointerMove, { signal: abortController.signal });
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  return { x: pos[0], y: pos[1] };
 }
--- workspaces/client/src/features/layout/hooks/useSubscribePointer.ts
+++ workspaces/client/src/features/layout/hooks/useSubscribePointer.ts
@@ -3,18 +3,4 @@ import { useEffect } from 'react';
 import { useStore } from '@wsh-2025/client/src/app/StoreContext';
 
 export function useSubscribePointer(): void {
-  const updatePointer = useStore((s) => s.features.layout.updatePointer);
-
-  useEffect(() => {
-    const abortController = new AbortController();
-
-    const handlePointerMove = (ev: MouseEvent) => {
-      updatePointer({ x: ev.clientX, y: ev.clientY });
-    };
-    window.addEventListener('pointermove', handlePointerMove, { signal: abortController.signal });
-
-    return () => {
-      abortController.abort();
-    };
-  }, []);
 }
