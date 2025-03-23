46f22753a94422637e5f8f5784f22dc66f7616fc
Koki Miita <miita@c-style.net>
2025-03-23 17:21:23 +0900

    perf: replace with css equivalent

--- workspaces/client/src/features/recommended/components/CarouselSection.tsx
+++ workspaces/client/src/features/recommended/components/CarouselSection.tsx
@@ -2,21 +2,17 @@ import { ElementScrollRestoration } from '@epic-web/restore-scroll';
 import { StandardSchemaV1 } from '@standard-schema/spec';
 import * as schema from '@wsh-2025/schema/src/api/schema';
 import { ArrayValues } from 'type-fest';
-import { useMergeRefs } from 'use-callback-ref';
 
 import { EpisodeItem } from '@wsh-2025/client/src/features/recommended/components/EpisodeItem';
 import { SeriesItem } from '@wsh-2025/client/src/features/recommended/components/SeriesItem';
 import { useCarouselItemWidth } from '@wsh-2025/client/src/features/recommended/hooks/useCarouselItemWidth';
-import { useScrollSnap } from '@wsh-2025/client/src/features/recommended/hooks/useScrollSnap';
 
 interface Props {
   module: Extract<ArrayValues<StandardSchemaV1.InferOutput<typeof schema.getRecommendedModulesResponse>>, { type: "carousel" }>;
 }
 
 export const CarouselSection = ({ module }: Props) => {
-  const containerRefForScrollSnap = useScrollSnap({ scrollPadding: 24 });
   const { ref: containerRefForItemWidth, width: itemWidth } = useCarouselItemWidth();
-  const mergedRef = useMergeRefs([containerRefForItemWidth, containerRefForScrollSnap]);
 
   return (
     <>
@@ -24,12 +20,12 @@ export const CarouselSection = ({ module }: Props) => {
         <h2 className="mb-[16px] w-full text-[22px] font-bold">{module.title}</h2>
         <div
           key={module.id}
-          ref={mergedRef}
-          className="relative mx-[-24px] flex flex-row gap-x-[12px] overflow-x-auto overflow-y-hidden pl-[24px] pr-[56px]"
+          ref={containerRefForItemWidth}
+          className="relative mx-[-24px] flex flex-row gap-x-[12px] overflow-x-auto overflow-y-hidden pl-[24px] pr-[56px] snap-x snap-mandatory scroll-px-[24px]"
           data-scroll-restore={`carousel-${module.id}`}
         >
           {module.items.map((item) => (
-            <div key={item.id} className="shrink-0 grow-0" style={{ width: `${itemWidth}px` }}>
+            <div key={item.id} className="shrink-0 grow-0 snap-start" style={{ width: `${itemWidth}px` }}>
               {item.series != null ? <SeriesItem series={item.series} /> : null}
               {item.episode != null ? <EpisodeItem episode={item.episode} /> : null}
             </div>
